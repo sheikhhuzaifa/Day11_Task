7cceb62dafde7a58d519b18dc52b361643537e68
sheikhhuzaifa <f190162@nu.edu.pk>
2023-07-07 16:39:05 +0500

    Validate and Validates

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
+  validate :validate_email_domain
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters are allowed" }
   has_many :books, dependent: :destroy
+  def validate_email_domain
+    unless email =~ /\A[\w+\-.]+@gmail\.com\z/i
+      errors.add(:email, "Only @gmail.com email addresses are allowed")
+    end
+  end
 end
